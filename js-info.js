8acf0fdb843b3f99fb7fa52b3d7039e393591eca
LiliyaShaih <83918023+LiliyaShaih@users.noreply.github.com>
2022-07-04 09:48:53 +0300

    homework3

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert('Брендан Эйх разработал язык программирования JavaScript');
+alert('JavaScript был создан за 10 дней');
+alert('JavaScript был создан в 1995 году');
+alert('JavaScript - это не Java');
+alert('Спецификация языка JavaScript называется ECMAScript');
